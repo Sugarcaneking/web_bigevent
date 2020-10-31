4f7bd1f47b916e671eef758eb6b32955cfea8823
Sugarcane <1159144089@qq.com>
2020-10-31 13:57:27 +0800

    完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,21 +1,21 @@
-$(function() {
-    // 点击“去注册账号”的链接
-    $('#link_reg').on('click', function() {
-      $('.login-box').hide()
-      $('.reg-box').show()
-    })
-  
-    // 点击“去登录”的链接
-    $('#link_login').on('click', function() {
-      $('.login-box').show()
-      $('.reg-box').hide()
-    })
-})
-form.verify({
+$(function () {
+  var layer = layui.layer
+  // 点击“去注册账号”的链接
+  $('#link_reg').on('click', function () {
+    $('.login-box').hide()
+    $('.reg-box').show()
+  })
+
+  // 点击“去登录”的链接
+  $('#link_login').on('click', function () {
+    $('.login-box').show()
+    $('.reg-box').hide()
+  })
+  layui.form.verify({
     // 自定义了一个叫做 pwd 校验规则
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
     // 校验两次密码是否一致的规则
-    repwd: function(value) {
+    repwd: function (value) {
       // 通过形参拿到的是确认密码框中的内容
       // 还需要拿到密码框中的内容
       // 然后进行一次等于的判断
@@ -25,9 +25,9 @@ form.verify({
         return '两次密码不一致！'
       }
     }
-})
+  })
   // 监听注册表单的提交事件
-$('#form_reg').on('submit', function(e) {
+  $('#form_reg').on('submit', function (e) {
     // 1. 阻止默认的提交行为
     e.preventDefault()
     // 2. 发起Ajax的POST请求
@@ -35,25 +35,25 @@ $('#form_reg').on('submit', function(e) {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
     }
-    $.post('http://ajax.frontend.itheima.net/api/reguser', data, function(res) {
+    $.post('/api/reguser', data, function (res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
-   layer.msg('注册成功，请登录！')
+      layer.msg('注册成功，请登录！')
       // 模拟人的点击行为
       $('#link_login').click()
     })
-})
-// 监听登录表单的提交事件
-$('#form_login').submit(function(e) {
+  })
+  // 监听登录表单的提交事件
+  $('#form_login').submit(function (e) {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
-      url: 'http://ajax.frontend.itheima.net/api/login',
+      url: '/api/login',
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('登录失败！')
         }
@@ -64,11 +64,6 @@ $('#form_login').submit(function(e) {
         location.href = '/index.html'
       }
     })
-})
-// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    options.url = 'http://ajax.frontend.itheima.net' + options.url
   })
+
+})
